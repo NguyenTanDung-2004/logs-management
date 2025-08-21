1fd0b750b769c01b4510f8b90a9957bdd303863a
Dung Tan Nguyen (3978) <dung.nguyen@kms.com.vn>
2025-08-21 15:50:49 +0700

    chore: setup log format

--- Readme.md
+++ Readme.md
@@ -1,14 +1,12 @@
-spring:
-  output:
-    ansi:
-      enabled: ALWAYS   # force enable ANSI colors
-
+# Logs - Management
+**1. application.yml file**
+```
 logging:
-  file:
-    name: logs/app.log
   pattern:
-    file: "%d{yyyy-MM-dd HH:mm:ss} %-5level %logger{36} - %msg%n"
     console: "%d{yyyy-MM-dd HH:mm:ss} %clr(%-5level){FATAL=red bold, ERROR=red, WARN=yellow, INFO=green, DEBUG=blue, TRACE=magenta} %clr(%logger{36}){cyan} - %msg%n"
   level:
     root: INFO
     com.example.LogsManagement: WARN
+```
+*- level.root:* it is default level for all package in system.<br>
+*- level.com.example.LogsManagement:* it is configured level for sepcific package
--- src/main/java/com/example/LogsMangement/LogsMangementApplication.java
+++ src/main/java/com/example/LogsMangement/LogsMangementApplication.java
@@ -1,13 +1,20 @@
 package com.example.LogsMangement;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @SpringBootApplication
-public class LogsMangementApplication {
+public class LogsMangementApplication implements CommandLineRunner {
+	private static final Logger log = LoggerFactory.getLogger(LogsMangementApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogsMangementApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.warn("nguyentandung");
+	}
 }
