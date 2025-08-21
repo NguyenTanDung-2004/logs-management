26c70f495e7ed154ad4ff4bb7f9aee24b757f133
Dung Tan Nguyen (3978) <dung.nguyen@kms.com.vn>
2025-08-21 13:39:17 +0700

    chore: setup project

---
+++ src/main/java/com/example/LogsMangement/LogsMangementApplication.java
@@ -0,0 +1,13 @@
+package com.example.LogsMangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsMangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogsMangementApplication.class, args);
+	}
+
+}
